5a686fa972161cb3c6aec0d16979b31eb8776280
daniellamarques <isabelmarques902@gmail.com>
2022-04-19 19:37:47 -0300

    second commit

--- src/DollarTeste.java
+++ src/DollarTeste.java
@@ -1,12 +1,18 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 public class DollarTeste{
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-     }}
-     
-     
-     
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+     }
+     @Test
+     public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+} 
