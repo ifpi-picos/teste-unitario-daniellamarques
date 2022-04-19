5a502e7cf181064db64df0b2daa7941716439644
daniellamarques <isabelmarques902@gmail.com>
2022-04-19 20:49:10 -0300

    bedroom

--- src/Money.java
+++ src/Money.java
@@ -1,15 +1,30 @@
-abstract class Money  {
+abstract class Money {
+    protected String currency;
     protected int amount;
-    static Money dollar(int amount){
-        return new Dollar(amount);
+    
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+     }
+     
+    static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
     }
-    static Money franc(int amount) {
-        return new Franc(amount);
-      }
-    abstract Money times(int multiplier);  
 
-    public boolean equals(Object object)  {
-       Money money = (Money) object;
-       return amount == money.amount && getClass().equals(money.getClass());
-    }   
+    static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+
+    abstract Money times(int multiplier);
+
+    String currency() {
+        return currency;
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && getClass().equals(money.getClass());
+    }
+
 }
